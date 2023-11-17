c8be258c8e1e0b150a1f9eae43f1da65d9cad712
Шестаков Егор Павлович <sep@mns.mnsspb.ru>
2023-11-17 10:49:02 +0300

    движение камеры и птицы

--- main.js
+++ main.js
@@ -10,17 +10,6 @@ const scene = new THREE.Scene();
 const gradientTexture = new THREE.TextureLoader().load("vivid-blurred-colorful-background.jpg");
 scene.background = gradientTexture;
 
-// Sphere 1
-const sizeBOX = PHY.boxConfig.size;
-const geometry = new THREE.BoxGeometry(sizeBOX[0], sizeBOX[1], sizeBOX[2]);
-const material = new THREE.MeshStandardMaterial({
-    color: '#fffb00',
-    roughness: 0.5,
-})
-const box = new THREE.Mesh(geometry, material);
-box.rotateX(0.1);
-scene.add(box);
-
 // Sphere 2
 const sizeSPHERE = PHY.sphereConfig.size;
 const geometry2 = new THREE.SphereGeometry(sizeSPHERE[0]);
@@ -39,19 +28,24 @@ const sizes = {
     height: window.innerHeight,
 }
 
+// Создаем вспомогательные оси
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
 // Lights
 const lightTOP = new THREE.HemisphereLight(0xffffff);
 lightTOP.position.set(0, 40, 30);
 scene.add(lightTOP);
 // Camera
 const camera = new THREE.PerspectiveCamera(
-    45, 
+    50, 
     sizes.width / sizes.height, 
     0.1, 
     1000
 );
-camera.position.z = 80;
-camera.position.y = 10;
+camera.position.z = -40;
+camera.position.x = -40;
+camera.position.y = 0;
 scene.add(camera);
 
 // Renderer
@@ -59,15 +53,17 @@ const canvas = document.querySelector('.webgl');
 const renderer = new THREE.WebGLRenderer({canvas});
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(2);
-renderer.render(scene,camera);
+renderer.render(scene, camera);
 
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
-// controls.enablePan = false;
-// controls.enableZoom = false;
+controls.enableRotate = true;
+controls.enablePan = false;
+controls.enableZoom = false;
 // controls.autoRotate = true;
 // controls.autoRotateSpeed = 5;
+
 
 // Resize
 window.addEventListener('resize', () => {
@@ -99,7 +95,7 @@ loader.load('bird.gltf', (gltf) => {
 
 // TimeLine
 const tl = gsap.timeline({defaults: {duration: 1 } });
-tl.fromTo(box.scale, { z: 0, x: 0, y: 0}, {z: 1, x: 1, y: 1});
+tl.fromTo(sphere.scale, { z: 0, x: 0, y: 0}, {z: 1, x: 1, y: 1});
 tl.fromTo("nav", { y: "-100%"}, { y: "0%"});
 
 // Mouse Animation
@@ -107,29 +103,25 @@ let mouseDown = false;
 let rgb = [];
 window.addEventListener('mousedown', () => { 
     mouseDown = true; // for mouse move
-    // PHY.sphereJump(sphere);
+    if (loadedGLTF) {
+        PHY.sphereJump(loadedGLTF.scene); // jump
+    }
 })
 window.addEventListener('mouseup', () => { mouseDown = false; })
 
 window.addEventListener('mousemove', (e) => {
-    if (mouseDown) {
-        rgb = [
-            Math.round((e.pageX / sizes.width) * 255),
-            Math.round((e.pageY / sizes.height) * 255),
-            150,
-        ];
-        // Animate colour
-        let newColor = new THREE.Color(`rgb(${rgb.join(',')})`);
-        gsap.to(box.material.color, {r: newColor.r, g:newColor.g, b:newColor.b})
-    }
 })
 
 const loop = () => {
     controls.update();
     PHY.updatePhysics();
-    PHY.updatePosition(sphere);
-    PHY.updateSphereRotation(sphere);
-    PHY.updateBoxRotation(box);
+    if (loadedGLTF) {
+        PHY.updatePosition(loadedGLTF.scene);
+        PHY.updateSphereRotation(loadedGLTF.scene);
+        controls.target.set(loadedGLTF.scene.position.x, loadedGLTF.scene.position.y, loadedGLTF.scene.position.z);
+        camera.position.z += 0.1;
+    }
+
     renderer.render(scene,camera);
     window.requestAnimationFrame(loop);
 }
--- phy.js
+++ phy.js
@@ -4,11 +4,10 @@ import "./oimo.js";
 const world = new OIMO.World({ 
     timestep: 1 / 30, 
     iterations: 8, 
-    broadphase: 3, // 1: brute force, 2: sweep and prune, 3: volume tree
+    broadphase: 2, // 1: brute force, 2: sweep and prune, 3: volume tree
     worldscale: 0.5, // scale full world
     random: true, // randomize sample
     info: false, // calculate statistic or not
-    gravity: [0, -1, 0],
 });
 
 // Create a physics sphere using oimo.js
@@ -22,31 +21,11 @@ const sphereConfig = {
     restitution: 0.6, // bounciness
 };
 
-// Create a physics sphere using oimo.js
-const boxConfig = {
-    type: 'box',
-    size: [16, 16, 64], // radius, height, depth (for spheres, only radius is used)
-    pos: [10, 0, 0], // initial position
-    rot: [1, 0, 0],
-    move: true, // allow movement
-    density: 1, // density of the object
-    friction: 0.2, // friction
-    restitution: 0.6, // bounciness
-};
-
 let sphere = world.add(sphereConfig);
-let box = world.add(boxConfig);
 
 const updatePosition = (obj) => {
     const position = sphere.getPosition();
-    const newPosition = new OIMO.Vec3(0, 0, 0);
-    box.setPosition(newPosition);
-    const contact = world.getContact( sphere, box );
-    if (contact && contact.touching === true) {
-        world.remove(sphere);
-        console.log(contact);
-    }
-
+    sphere.position.z += 0.1;
     // if out of height
     if (position.y < -40) {
         // Remove the sphere from the world
@@ -56,11 +35,11 @@ const updatePosition = (obj) => {
         const newBodyConfig = {
             type: 'sphere',
             size: [3, 3, 3],
-            pos: [5, 35, 0],  // New initial position
-            move: true,
-            density: 1,
-            friction: 0.2,
-            restitution: 0.2,
+            pos: [0, 35, 0],  // New initial position
+            move: true, // allow movement
+            density: 0.1, // density of the object
+            friction: 0.2, // friction
+            restitution: 0.6, // bounciness
         };
 
         // Add the new sphere to the world
@@ -70,31 +49,27 @@ const updatePosition = (obj) => {
         sphere = newBody;
     }
 
-    obj.position.set(position.x, position.y, position.z);
+    obj.position.set(position.x, position.y, sphere.position.z);
     return position;
-}
-
-const updateBoxRotation = (obj) => {
-    const rotation = box.getQuaternion();
-    obj.quaternion.set(rotation.x, rotation.y, rotation.z, rotation.w);
-    return rotation;
 }
 
 const updateSphereRotation = (obj) => {
     const rotation = sphere.getQuaternion();
-    obj.quaternion.set(rotation.x, rotation.y, rotation.z, rotation.w);
+    obj.quaternion.set(0, 0, 0, 0);
     return rotation;
 }
 
 const sphereJump = (obj) => {
+    console.log("JUMP")
     const position = new OIMO.Vec3(0, 0, 0);
-    const force = new OIMO.Vec3(0, 1225, 0);
+    const force = new OIMO.Vec3(0, 2225, 0);
     sphere.applyImpulse(position, force);
-    obj.position.set(position.x, position.y, position.z);
+    const newPosition = sphere.getPosition();
+    obj.position.set(newPosition.x, newPosition.y, newPosition.z);
 };
 
 const updatePhysics = () => {
     world.step();
 }
 
-export { world, sphereConfig, boxConfig, updatePosition, updateBoxRotation, updateSphereRotation, updatePhysics, sphereJump };
+export { world, sphereConfig, updatePosition, updateSphereRotation, updatePhysics, sphereJump };
